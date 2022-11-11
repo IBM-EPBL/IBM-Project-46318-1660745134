034282d488169ec1b489fac8432df0c17a12030a
sarumathei <75022372+sarumathei@users.noreply.github.com>
2022-11-11 09:48:41 +0530

    Delete dense.py

--- Project developemt phase/Sprint 3/Model Building/Adding CNN layers/Adding Dense Layer/dense.py
+++
@@ -1,4 +0,0 @@
-model.add(Dense(units=128,activation = 'relu'))
-model.add(Dense(units = 64, activation = 'relu'))
-model.add(Dense(units = 32, activation = 'relu'))
-model.add(Dense(units = 6, activation = 'softmax'))
